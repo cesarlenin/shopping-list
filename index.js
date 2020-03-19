5d74c4adc11b2d430183f746b8e822a774bc8ce1
Cesar Morales <cesarlenin2@live.com>
2020-03-18 17:13:51 -0700

    added toggle and delete functions

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 //Your challenge is to create an application that allows users to add, check, uncheck,
 // and remove items from a shopping list. 
+$(function () {
 
-$('.addButton').click(function(event){
+$('form').on('click','button', event => {
     event.preventDefault();
     let outPut =$('input').val();
-    //console.log(outPut);
     $('.shopping-list').append(
         `<li>
         <span class="shopping-item">${outPut}</span>
@@ -19,4 +19,16 @@ $('.addButton').click(function(event){
         </div>
       </li>`
     )
+})
+
+$('.shopping-list').on('click','.shopping-item-toggle', event => {
+    event.preventDefault();
+    $(event.currentTarget).parents('li').find('.shopping-item').toggleClass('shopping-item__checked');
+})
+
+$('.shopping-list').on('click','.shopping-item-delete', event => {
+    event.preventDefault();
+    $(event.currentTarget).parents('li').remove();
+})
+
 })
